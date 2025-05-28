b9624ee24d9891905fd0f4e82b9aea8d2affd534
mottasimsadi <mottasimsadi@gmail.com>
2025-05-29 03:51:30 +0600

    Created add coffee form to get coffee data

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 
 const MainLayout = () => {
     return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
+      <div>
+        <Header></Header>
+        <div className='max-w-7xl mx-auto'>
+          <Outlet></Outlet>
         </div>
+      </div>
     );
 };
 
