c4a7fcc735a93173daf75224984996ab2b9610cf
mottasimsadi <mottasimsadi@gmail.com>
2025-05-31 03:38:46 +0600

    Display coffee data and create delete method

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+
+const CoffeeCard = ({ coffee }) => {
+    const { _id, name, price, quantity, photo } = coffee;
+
+    const handleDelete = (_id) =>{
+        // console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            
+            if (result.isConfirmed) {
+                // console.log(result.isConfirmed);
+            //   Swal.fire({
+            //     title: "Deleted!",
+            //     text: "Your file has been deleted.",
+            //     icon: "success"
+            //   });
+            }
+          });
+
+    }
+
+    return (
+        <div className="card card-side bg-base-100 shadow-sm border-2">
+            <figure>
+                <img
+                    src={photo}
+                    alt="Pictures" />
+            </figure>
+            <div className="flex w-full justify-around">
+                <div className='mt-14 md:mt-12'>
+                    <h2>{name}</h2>
+                    <p>Price: {price}</p>
+                    <p>Quantity: {quantity}</p>
+                </div>
+                <div className="card-actions justify-end">
+                    <div className="join join-vertical mt-8 md:mt-4 space-y-2">
+                        <button className="btn join-item">View</button>
+                        <button className="btn join-item">Edit</button>
+                        <button onClick={ () => handleDelete(_id)} className="btn join-item">X</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeCard;
