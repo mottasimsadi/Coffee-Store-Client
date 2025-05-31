284107fe57d23916768f305b1d2322ba36ecb21c
mottasimsadi <mottasimsadi@gmail.com>
2025-06-01 01:00:10 +0600

    Created users route and display users in a table

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import CoffeeDetails from "./components/CoffeeDetails.jsx";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
 import AuthProvider from "./contexts/AuthProvider.jsx";
+import Users from "./components/Users.jsx";
 
 const router = createBrowserRouter([
   {
@@ -43,6 +44,11 @@ const router = createBrowserRouter([
       {
         path: 'signup',
         Component: SignUp
+      },
+      {
+        path: 'users',
+        loader: () => fetch('http://localhost:3000/users'),
+        Component: Users
       }
     ],
   },
