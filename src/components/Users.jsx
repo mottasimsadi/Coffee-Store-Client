22dc8592995821ee57e7f6161b4c9d369abbbeb4
mottasimsadi <mottasimsadi@gmail.com>
2025-06-01 01:19:35 +0600

    Delete user update state and created sign in form

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
   const [users, setUsers] = useState(initialUsers);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              const remainingUsers = users.filter((user) => user._id !== id);
+              setUsers(remainingUsers);
+
+              // TODO Delete user from firebase
+
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your user has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <h2 className="text-3xl">Users: {users.length}</h2>
@@ -43,7 +78,10 @@ const Users = () => {
                 <th>
                   <button className="btn btn-xs">V</button>
                   <button className="btn btn-xs">E</button>
-                  <button className="btn btn-xs">
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-xs"
+                  >
                     X
                   </button>
                 </th>
